e1155ec79640206456f46114461e0100deffdcfa
Leslie Shea <leslieyshea@gmail.com>
2020-10-05 00:52:27 -0700

    added forms module for add to do component

---
+++ src/app/add-to-do/add-to-do.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-to-do',
+  templateUrl: './add-to-do.component.html',
+  styleUrls: ['./add-to-do.component.css']
+})
+export class AddToDoComponent implements OnInit {
+  title: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
 import { ToDoItemComponent } from './to-do-item/to-do-item.component';
 import { HeaderComponent } from './header/header.component';
+import { AddToDoComponent } from './add-to-do/add-to-do.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDoListComponent,
     ToDoItemComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddToDoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule // have to add this here because it's a module
+    HttpClientModule, // have to add this here because it's a module
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
