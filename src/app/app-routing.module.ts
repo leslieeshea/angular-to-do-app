479fdba7c6647c19ece556e37624dbdf4d5e99d3
Leslie Shea <leslieyshea@gmail.com>
2020-10-05 01:10:32 -0700

    routing works for about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  { path: '', component: ToDoListComponent }
+  { path: '', component: ToDoListComponent },
+  { path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
